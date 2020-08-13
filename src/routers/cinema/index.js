5a6d58d4d70e63e9894d009fa24602940ae5d591
pj1653109 <pj18616017479>
2020-08-13 10:51:43 +0800

    add createComponents

---
+++ src/routers/cinema/index.js
@@ -0,0 +1,15 @@
+export default {
+    path :'/cinema',
+    component:() => import('@/views/Cinema'),
+    // children:[
+    //     {
+    //         path:'CiList',
+    //         component:() => import('@/components/CiList')
+    //     },
+    //     {
+    //         path:'/cinema',
+    //         redirect : '/cinema/CiList'
+    //     }
+    // ]
+    
+}
